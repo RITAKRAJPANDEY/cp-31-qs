2fca940d18fd553c819face1caa30b4f1748ebc8
RITAKRAJPANDEY <rp2194679@gmail.com>
2026-05-27 21:54:29 +0530

    1873C

---
+++ 1873C.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+void t_case(){
+    vector<string>s(100);
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cin>>s[j];
+            if (s[j]=="X")
+            {
+                if(i<5){
+                    
+                }
+            }
+            
+        }
+        
+    }
+    
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        t_case();
+    }
+    
+}
